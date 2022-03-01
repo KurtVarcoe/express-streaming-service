697679ddcb26f4486144f41c124bfa25ea13e6a4
Kurt <kurtvarcoe@gmail.com>
2022-03-01 20:55:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const genre = [
+    { id: 1, genre: genre1 },
+    { id: 2, genre: genre2 },
+    { id: 3, genre: genre3 },
+    { id: 4, genre: genre4 }
+];
