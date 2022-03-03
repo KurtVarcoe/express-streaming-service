0dc6910ab11010189415c55854613ebf4223ac0b
Kurt <kurtvarcoe@gmail.com>
2022-03-03 05:39:26 +0200

    corrected error in get req

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 
-const genre = [
-    { id: 1, genre: genre1 },
-    { id: 2, genre: genre2 },
-    { id: 3, genre: genre3 },
-    { id: 4, genre: genre4 }
+const genres = [
+    { id: 1, genre: "genre1" },
+    { id: 2, genre: "genre2" },
+    { id: 3, genre: "genre3" },
+    { id: 4, genre: "genre4" }
 ];
 
 
-app.get('/api/genres/', (res, req) => {
-    res.send(genre);
-    res.end();
+app.get('/api/genres/', (req, res) => {
+    res.send(genres);
 });
 
 const port = process.env.PORT || 3000;
