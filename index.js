db6322a049ea61b53146205659e4c0037d9f1eb9
Kurt <kurtvarcoe@gmail.com>
2022-03-04 20:42:22 +0200

    created post request

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
-const genres = [
-    { id: 1, genre: "genre1" },
-    { id: 2, genre: "genre2" },
-    { id: 3, genre: "genre3" },
-    { id: 4, genre: "genre4" }
-];
+app.use(express.json());
 
+const genres = [
+    { id: 1, name: "genre1" },
+    { id: 2, name: "genre2" },
+    { id: 3, name: "genre3" },
+    { id: 4, name: "genre4" },
+];
 
 app.get('/api/genres/', (req, res) => {
     res.send(genres);
@@ -16,6 +17,15 @@ app.get('/api/genres/', (req, res) => {
 app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(c => c.id === parseInt(req.params.id));
     if(!genre) res.status(404).send('The genre with the given ID could not be found');
+    res.send(genre);
+});
+
+app.post('api/genres', (req,res) => {
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name 
+    };
+    genres.push(genre);
     res.send(genre);
 });
 
